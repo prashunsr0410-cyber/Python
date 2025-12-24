2675b7ea228fd1afa59aa3e46800666c250d4775
Prashun Srivastava <225806462+prashunsr0410-cyber@users.noreply.github.com>
2025-12-24 16:35:49 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,11 @@
+Name = input("Enter your name: ").strip().title()
+print(f"Hello, {Name}!")
+# This is a simple Python script that prints "hello world" to the console.
+# It serves as a basic example of how to write and run a Python script.
+
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+division = num_1 // num_2
+print(f"{num_1} divided by {num_2} is {division}")
+
+print("fun" * 5, sep="___")
